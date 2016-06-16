b720f548dbc4a0f533b6c16333dcc03ad7e0ae4d
Anthony Benavente <anthony.m.benavente@gmail.com>
2016-06-16 14:18:52 -0400

    Wrote out countries to JSON instead of txt

--- listregions.py
+++ listregions.py
@@ -1,15 +1,17 @@
 import pandas as pd
+import json
 
 def main():
     df = pd.read_csv(r'data\\SurveyData_Text_Cleaned.csv')
     countries = { }
     for index, row in df.iterrows():
-        country = row['Q12.5']
-        if country in countries:
-            countries[country] += 1
-        else:
-            countries[country] = 1
-    print countries
+        if index > 0:
+            country = row['Q12.5']
+            if country in countries:
+                countries[country] += 1
+            else:
+                countries[country] = 1
+    print json.dumps(countries)
 
 if __name__=='__main__':
     main() 
