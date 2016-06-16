02c66ba0b78a3dea909078e3ea5df644e0f2b73a
Anthony Benavente <anthony.m.benavente@gmail.com>
2016-06-16 14:15:24 -0400

    Created script to generate list of regions + counts per region

--- listregions.py
+++ listregions.py
@@ -2,7 +2,14 @@ import pandas as pd
 
 def main():
     df = pd.read_csv(r'data\\SurveyData_Text_Cleaned.csv')
-    print "Read in the csv file"
+    countries = { }
+    for index, row in df.iterrows():
+        country = row['Q12.5']
+        if country in countries:
+            countries[country] += 1
+        else:
+            countries[country] = 1
+    print countries
 
 if __name__=='__main__':
-    main()
+    main() 
